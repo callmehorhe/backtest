08a4c078e7926ef9a31fe4ff77f908159ef5cbd3
callmehorhe <91099985+callmehorhe@users.noreply.github.com>
2022-02-08 13:12:29 +0300

    ...

--- pkg/repository/authpostgres.go
+++ pkg/repository/authpostgres.go
@@ -1,9 +1,7 @@
 package repository
 
 import (
-	"log"
-
-	"github.com/callmehorhe/backtest"
+	serv "github.com/callmehorhe/backtest"
 	"gorm.io/gorm"
 )
 
@@ -11,27 +9,26 @@ type AuthPostgres struct {
 	db *gorm.DB
 }
 
-func NewAuthPostgres(db *gorm.DB) *AuthPostgres{
+func NewAuthPostgres(db *gorm.DB) *AuthPostgres {
 	return &AuthPostgres{
 		db: db,
 	}
 }
 
-func (r *AuthPostgres) CreateUser(user serv.User)(int, error){
+func (r *AuthPostgres) CreateUser(user serv.User) (int, error) {
 	var id int
 	row := r.db.Create(&user)
-	if err := row.Error; err != nil{
+	if err := row.Error; err != nil {
 		return 0, err
 	}
 	row.Select("id_user").Scan(&id)
 	return id, nil
 }
 
-func (r *AuthPostgres) GetUser(email, password string) (serv.User, error){
+func (r *AuthPostgres) GetUser(email, password string) (serv.User, error) {
 	var user serv.User
 	user.Email = email
 	user.Password = password
 	err := r.db.Select("id_user").Where("email = ? AND password = ?", email, password).Take(&user).Error
-	log.Print(user)
 	return user, err
 }
